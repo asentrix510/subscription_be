74b7f0a2b23131619def5e2cd82b50bfec927a0d
asentrix510 <asentrix510@gmail.com>
2026-01-30 22:38:52 +0530

    added mongodb access

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,4 @@
-import {Router} from express;
+import {Router} from "express";
 const subscriptionRouter=Router() //instantiation
 subscriptionRouter.get('/',(req,res)=>{
     res.send("Get all subscriptions")
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,19 +1,25 @@
-import {Router} from express;
-const userRouter=Router();
-userRouter.get('/',(req,res)=>{
- res.send("Get all users")
-})
-userRouter.get('/:id',(req,res)=>{
- res.send("Get user details")
-})
-userRouter.post('/',(req,res)=>{
- res.send("Create a new user")
-})
-userRouter.put('/:id',(req,res)=>{
- res.send("Update user")
-})
-userRouter.delete('/:id',(req,res)=>{
- res.send("Delete user")
-})
+import { Router } from "express";
 
-export default userRouter
+const userRouter = Router();
+
+userRouter.get("/", (req, res) => {
+  res.send("Get all users");
+});
+
+userRouter.get("/:id", (req, res) => {
+  res.send("Get user details");
+});
+
+userRouter.post("/", (req, res) => {
+  res.send("Create a new user");
+});
+
+userRouter.put("/:id", (req, res) => {
+  res.send("Update user");
+});
+
+userRouter.delete("/:id", (req, res) => {
+  res.send("Delete user");
+});
+
+export default userRouter;
